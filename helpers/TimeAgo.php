c8ac53680d74a3bec5ef26ca60b45799e5d943bb
ilya <zaytsew.ilya@gmail.com>
2013-12-20 01:07:06 +0400

    init

---
+++ helpers/TimeAgo.php
@@ -0,0 +1,65 @@
+<?php
+/**
+ * Class TimeAgo
+ *
+ * Yii1 helper class
+ * Formats date in seconds to passed time
+ *
+ * @license same as Yii itself
+ * @author Ilya Zaytsev <zaytsew.ilya@gmail.com>
+ *
+ * @version 0.1
+ *
+ * @link
+ */
+class TimeAgo
+{
+	const WEEK = 604800;
+	const DAY = 86400;
+	const HOUR = 3600;
+	const MINUTE = 60;
+
+	/**
+	 * Formats given time in seconds into string {n} time ago
+	 *
+	 * @param integer $time
+	 * @param null|integer $now
+	 * @return string
+	 */
+	public static function f($time, $now=null)
+	{
+		if(!isset($now))
+		{
+			$now = time();
+		}
+
+		$diff = $now - $time;
+
+		if($diff < self::WEEK && $diff >= self::DAY)
+		{
+			return Yii::t('time_ago','{n} day ago|{n} days ago',array(floor($diff / self::DAY)));
+		}
+		elseif($diff < self::DAY && $diff >= self::HOUR)
+		{
+			return Yii::t('time_ago','{n} hour ago|{n} hours ago',array(floor($diff / self::HOUR)));
+		}
+		elseif($diff < self::HOUR && $diff >= self::MINUTE)
+		{
+			return Yii::t('time_ago','{n} minute ago|{n} minutes ago',array(floor($diff / self::MINUTE)));
+		}
+		elseif($diff < self::MINUTE) {
+			return Yii::t('time_ago','{n} second ago|{n} seconds ago',array($diff));
+		}
+		elseif($diff == 0)
+		{
+			return 'Только что';
+		}
+		else
+		{
+			$date = new CDateFormatter(Yii::app()->locale);
+			$pattern = date('Y', $now) == date('Y', $time) ? 'd MMM' : 'd MMM y';
+			return $date->format($pattern, $time);
+		}
+	}
+}
+
---
+++ messages/ru_ru/time_ago.php
@@ -0,0 +1,7 @@
+<?php
+return array(
+	'{n} day ago|{n} days ago' => '{n} день назад|{n} дня назад|{n} дней назад',
+	'{n} hour ago|{n} hours ago' => '{n} час назад|{n} часа назад|{n} часов назад',
+	'{n} minute ago|{n} minutes ago' => '{n} минуту назад|{n} минуты назад|{n} минут назад',
+	'{n} second ago|{n} seconds ago' => '{n} секунду назад|{n} секунды назад|{n} секунд назад'
+);
